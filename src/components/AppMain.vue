e862447762e84c1581e6eb48c913a84f4f188e14
MatteoSanson <matteo.sanson@gmail.com>
2023-12-08 17:56:05 +0100

    feat: main

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -5,12 +5,26 @@ export default {
 </script>
 
 <template>
-    <main>main prova</main>
+    <main>
+        <div class="container-main">
+            <p>
+                --&gt; Content goes here &lt;--
+            </p>
+        </div>
+    </main>
 </template>
 
 
 <style scoped>
 main {
     background-color: #1c1c1c;
+}
+
+.container-main {
+    width: 80%;
+    margin: 0 auto;
+    height: 130px;
+    display: flex;
+    align-items: center;
 }
 </style>

acba8a9fdfa6707fc5360cd6b267173a1de931ee
MatteoSanson <matteo.sanson@gmail.com>
2023-12-09 17:21:02 +0100

    feat: jumbotron

--- src/components/Jumbotron/AppJumbotron.vue
+++ src/components/Jumbotron/AppJumbotron.vue
@@ -1,6 +1,42 @@
 <script>
 export default {
-    name: 'Jumbotronnn',
+    name: 'Jumbotron',
+    data() {
+        return {
+            dcList: [
+                {
+                    name: 'digital comics',
+                    img: '/img/buy-comics-digital-comics.png',
+                    link: '#',
+                    target: '_self',
+                },
+                {
+                    name: 'dc merchandise',
+                    img: '/img/buy-comics-merchandise.png',
+                    link: '#',
+                    target: '_self',
+                },
+                {
+                    name: 'subscription',
+                    img: '/img/buy-comics-subscriptions.png',
+                    link: '#',
+                    target: '_self',
+                },
+                {
+                    name: 'comic shop locator',
+                    img: '/img/buy-comics-shop-locator.png',
+                    link: '#',
+                    target: '_self',
+                },
+                {
+                    name: 'dc power visa',
+                    img: '/img/buy-dc-power-visa.svg',
+                    link: '#',
+                    target: '_self',
+                },
+            ]
+        }
+    }
 };
 </script>
 
@@ -8,12 +44,19 @@ export default {
     <div class="jumbo">
         <div class="container">
             <ul>
-                <li>ciao</li>
-                <li>ciao</li>
-                <li>ciao</li>
-                <li>ciao</li>
-                <li>ciao</li>
-                <li>ciao</li>
+                <li v-for="li in dcList" class="point">
+                    <div class="logo">
+                        <a :href="li.link" :target="li.target">
+                            <img :src="li.img" :alt="li.name">
+                        </a>
+                    </div>
+                    <div class="near-logo">
+                        <a :href="li.link" :target="li.target">
+                            {{ li.name.toUpperCase() }}
+                        </a>
+                    </div>
+                </li>
+
             </ul>
         </div>
     </div>
@@ -25,17 +68,47 @@ export default {
 
 .jumbo {
     background-color: $onhover;
+
+    a {
+        text-decoration: none;
+        color: $primary;
+
+        &:hover {
+            color: gainsboro;
+        }
+    }
 }
 
 .container {
     width: 80%;
     height: 150px;
     margin: 0 auto;
+    display: flex;
+    align-items: center;
 
     ul {
+        width: 100%;
         list-style: none;
         display: flex;
         justify-content: space-around;
+    }
+}
+
+.point {
+    display: flex;
+    align-items: center;
+    gap: 15px;
+}
+
+.logo {
+    width: 40px;
+    height: 40px;
+
+    a img {
+        width: 100%;
+        height: 100%;
+        object-fit: contain;
+        display: block;
     }
 }
 </style>
